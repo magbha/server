e32c506807b63239f4ae108c922953a1aa2a52aa
magbha <abdelghanibha@gmail.com>
2023-03-08 16:03:30 +0000

    arr

--- server.js
+++ server.js
@@ -1,29 +1,27 @@
 const express = require("express");
 const app = express();
-const cors = require("cors")
+const cors = require("cors");
 
 require("dotenv").config();
 
 const corsOptions = require("./Config/Cors");
-app.use(express.json())
+app.use(express.json());
 
 const ConnectDB = require("./Config/ConnectDB");
 ConnectDB();
 
+app.use(cors({ origin: ["https://st-room-inventory.onrender.com"] }));
+app.use("/api/user", require("./Routes/user"));
+app.use("/api/branch", require("./Routes/branch"));
+app.use("/api/item", require("./Routes/item"));
+app.get("/", (req, res) => {
+  res.send("API is Running...");
+});
 
+const PORT = process.env.PORT || 5421;
 
-//app.use(cors())
-app.use("/api/user" , require("./Routes/user"))
-app.use("/api/branch" , require("./Routes/branch"))
-app.use("/api/item" , require("./Routes/item"))
-app.get("/" , (req , res) => {
-    res.send("API is Running...")
-})
-
-
-const PORT = process.env.PORT || 5421
-
-app.listen(PORT , error => {
-    error ? console.error(`failed to connect ${error}`) :
-    console.log(`server is running on port : ${PORT}`)
-})
+app.listen(PORT, (error) => {
+  error
+    ? console.error(`failed to connect ${error}`)
+    : console.log(`server is running on port : ${PORT}`);
+});
