cc65fad925c135a53d4cbd00a1debf17109b8cd6
magbha <abdelghanibha@gmail.com>
2023-03-08 15:37:44 +0000

    i

--- Config/Cors.js
+++ Config/Cors.js
@@ -9,13 +9,7 @@ const allowedOrigins = [
 ]
 
 const corsOptions = {
-  origin: (origin, callback) => {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed M by CORS"));
-    }
-  },
+  origin: "*",
   credentials: true,
   optionsSuccessStatus: 200,
 };
