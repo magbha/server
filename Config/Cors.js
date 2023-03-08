7cdc52df9fd6ddc39997aa57adbdbefe05d7af5c
magbha <abdelghanibha@gmail.com>
2023-03-08 03:10:28 +0000

    lr

--- Config/Cors.js
+++ Config/Cors.js
@@ -1,17 +1,10 @@
 
-const allowedOrigins = [
-    'https://st-room-inventory.onrender.com/'
-]
+
+
 
 const corsOptions = {
-    origin: (origin, callback) => {
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'))
-        }
-    },
-    credentials: false,
+    origin: 'https://st-room-inventory.onrender.com' ,
+    credentials: true,
     optionsSuccessStatus: 200,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: ['Content-Type', 'Authorization']
