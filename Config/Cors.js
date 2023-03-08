e08811aa869805a28969bb33dcd4d1b368320cec
magbha <abdelghanibha@gmail.com>
2023-03-08 15:29:24 +0000

    M

--- Config/Cors.js
+++ Config/Cors.js
@@ -1,11 +1,23 @@
-
-
-
+const allowedOrigins = [
+    'https://st-room-inventory.onrender.com/',
+    'https://st-room-inventory.onrender.com/sign-in',
+    'https://st-room-inventory.onrender.com/sign-up',
+    'https://st-room-inventory.onrender.com/dash',
+    'https://st-room-inventory.onrender.com/branch',
+    'https://st-room-inventory.onrender.com/products',
+    'https://st-room-inventory.onrender.com/team',
+]
 
 const corsOptions = {
-    origin: 'https://st-room-inventory.onrender.com' ,
+  origin: (origin, callback) => {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed M by CORS"));
+    }
+  },
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
 
-    
-}
-
-module.exports = corsOptions
+module.exports = corsOptions;
