c1b93f8c947e2730be6835656c933ce42d7724cf
magbha <abdelghanibha@gmail.com>
2023-03-08 17:36:46 +0000

    kkkk

--- Config/Cors.js
+++ Config/Cors.js
@@ -1,18 +1,17 @@
 const allowedOrigins = [
-    'https://st-room-inventory.onrender.com/',
-    'https://st-room-inventory.onrender.com/sign-in',
-    'https://st-room-inventory.onrender.com/sign-up',
-    'https://st-room-inventory.onrender.com/dash',
-    'https://st-room-inventory.onrender.com/branch',
-    'https://st-room-inventory.onrender.com/products',
-    'https://st-room-inventory.onrender.com/team',
-]
+  "https://st-room-inventory.onrender.com/",
+  "https://st-room-inventory.onrender.com/sign-in",
+  "https://st-room-inventory.onrender.com/sign-up",
+  "https://st-room-inventory.onrender.com/dash",
+  "https://st-room-inventory.onrender.com/branch",
+  "https://st-room-inventory.onrender.com/products",
+  "https://st-room-inventory.onrender.com/team",
+];
 
 const corsOptions = {
   origin: ["https://st-room-inventory.onrender.com"],
   optionsSuccessStatus: 200,
   credentials: true,
-  allowedHeaders : ['Content-Type', 'Authorization']
 };
 
 module.exports = corsOptions;
