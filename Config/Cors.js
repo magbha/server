81437a68f9e7ca836817a06dd4efd7ab198c38be
magbha <abdelghanibha@gmail.com>
2023-03-08 15:10:48 +0000

    credentials: true,

--- Config/Cors.js
+++ Config/Cors.js
@@ -4,7 +4,6 @@
 
 const corsOptions = {
     origin: 'https://st-room-inventory.onrender.com' ,
-    credentials: true,
     optionsSuccessStatus: 200,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: ['Content-Type', 'Authorization']
